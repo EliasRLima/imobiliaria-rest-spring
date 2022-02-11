94fccffd857f82d7c80dafb5159abde0ec463b71
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-11 20:51:00 -0300

    classe_cliente

---
+++ src/main/java/ifma/edu/imobiliaria/model/Cliente.java
@@ -0,0 +1,104 @@
+package ifma.edu.imobiliaria.model;
+
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import java.util.Objects;
+
+@Entity
+public class Cliente implements Serializable {
+
+    private static  final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue
+    private Integer id;
+
+    @NotNull @NotEmpty @Length(max = 255)
+    private String nome_cliente;
+
+    @NotNull @NotEmpty @Length(min = 11, max = 11)
+    private String cpf;
+
+    @NotEmpty @Length(min = 8, max = 12)
+    private String telefone;
+
+    @NotEmpty @Length(max = 100)
+    private String email;
+
+    private String dt_nascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome_cliente() {
+        return nome_cliente;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        this.nome_cliente = nome_cliente;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDt_nascimento() {
+        return dt_nascimento;
+    }
+
+    public void setDt_nascimento(String dt_nascimento) {
+        this.dt_nascimento = dt_nascimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome_cliente + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
