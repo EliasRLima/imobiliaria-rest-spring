42a2c654ee3a593621f9de20c896064aa635b453
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-11 21:01:45 -0300

    Create ClienteService.java

---
+++ src/main/java/ifma/edu/imobiliaria/service/ClienteService.java
@@ -0,0 +1,52 @@
+package ifma.edu.imobiliaria.service;
+
+import ifma.edu.imobiliaria.model.Cliente;
+import ifma.edu.imobiliaria.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> todos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscaPor(Integer id) {
+        return clienteRepository.findById(id);
+    }
+
+    public List<Cliente> buscaPor(String nome) {
+        return clienteRepository.findByNomeContaining(nome);
+    }
+
+    public Page<Cliente> buscaPor(String nome, Pageable paginacao) {
+        return clienteRepository.findByNomeContaining(nome, paginacao);
+    }
+
+    @Transactional
+    public Cliente salva(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+
+    @Transactional
+    public void removePelo(Integer id) {
+        clienteRepository.deleteById(id);
+    }
+
+    public Page<Cliente> buscaCom(Pageable paginacao) {
+        return clienteRepository.findAll(paginacao );
+    }
+}
