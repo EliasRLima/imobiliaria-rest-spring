960eef17b411742d2d80d769375d5acf384d8eaa
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-11 21:02:43 -0300

    Update ClienteRepository.java

--- src/main/java/ifma/edu/imobiliaria/repository/ClienteRepository.java
+++ src/main/java/ifma/edu/imobiliaria/repository/ClienteRepository.java
@@ -2,7 +2,12 @@ package ifma.edu.imobiliaria.repository;
 
 import ifma.edu.imobiliaria.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+    List<Cliente> findByNomeContaining(String nome );
+    Page<Cliente> findByNomeContaining(String nome, Pageable paginacao);
 }
