67efb77da7f62419aa09785d0ad77a7f667e5e46
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-13 20:58:54 -0300

    update imovel

--- src/main/java/ifma/edu/imobiliaria/model/Imovel.java
+++ src/main/java/ifma/edu/imobiliaria/model/Imovel.java
@@ -1,4 +1,134 @@
 package ifma.edu.imobiliaria.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 public class Imovel {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Length(max = 50)
+    private String tipo_imovel;
+
+    @Length(max = 255)
+    private String endereco;
+
+    @Length(max = 10)
+    private String cep;
+
+    private Integer dormitorios;
+    private Integer banheiros;
+    private Integer suites;
+    private Integer metragem;
+    private Double valor_aluguel_sug;
+    private String obs;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipo_imovel() {
+        return tipo_imovel;
+    }
+
+    public void setTipo_imovel(String tipo_imovel) {
+        this.tipo_imovel = tipo_imovel;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getDormitorios() {
+        return dormitorios;
+    }
+
+    public void setDormitorios(Integer dormitorios) {
+        this.dormitorios = dormitorios;
+    }
+
+    public Integer getBanheiros() {
+        return banheiros;
+    }
+
+    public void setBanheiros(Integer banheiros) {
+        this.banheiros = banheiros;
+    }
+
+    public Integer getSuites() {
+        return suites;
+    }
+
+    public void setSuites(Integer suites) {
+        this.suites = suites;
+    }
+
+    public Integer getMetragem() {
+        return metragem;
+    }
+
+    public void setMetragem(Integer metragem) {
+        this.metragem = metragem;
+    }
+
+    public Double getValor_aluguel_sug() {
+        return valor_aluguel_sug;
+    }
+
+    public void setValor_aluguel_sug(Double valor_aluguel_sug) {
+        this.valor_aluguel_sug = valor_aluguel_sug;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Imovel imovel = (Imovel) o;
+        return Objects.equals(id, imovel.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", endereco='" + endereco + '\'' +
+                ", valor='" + valor_aluguel_sug + '\'' +
+                '}';
+    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@ insert into cliente(id, nome, cpf, telefone, email, dt_nascimento) values (1, 'A
 insert into cliente(id, nome, cpf, telefone, email, dt_nascimento) values (2, 'Elias', '11111111111', '989999999', 'elias@email', '11-02-2022');
 insert into cliente(id, nome, cpf, telefone, email, dt_nascimento) values (3, 'Italo Auau', '11111111111', '989999999', 'auau@email', '11-02-2022');
 
-insert into imoveis(id, tipo_imovel, endereco, cep, dormitorios, banheiros, suites, metragem, valor_aluguel_sug, obs ) values (1, 'residencia', 'numero 103 - rua 01 - bairro 02 - Cidade 03', '00000-000', 2, 1, 0, 50, 200.0, 'A casa tem um fantasma (mas é gente boa)');
-insert into imoveis(id, tipo_imovel, endereco, cep, dormitorios, banheiros, suites, metragem, valor_aluguel_sug, obs ) values (2, 'residencia', 'numero 104 - rua 01 - bairro 02 - Cidade 03', '00000-000', 2, 1, 0, 50, 200.0, null);
+insert into imovel(id, tipo_imovel, endereco, cep, dormitorios, banheiros, suites, metragem, valor_aluguel_sug, obs ) values (1, 'residencia', 'numero 103 - rua 01 - bairro 02 - Cidade 03', '00000-000', 2, 1, 0, 50, 200.0, 'A casa tem um fantasma (mas é gente boa)');
+insert into imovel(id, tipo_imovel, endereco, cep, dormitorios, banheiros, suites, metragem, valor_aluguel_sug, obs ) values (2, 'residencia', 'numero 104 - rua 01 - bairro 02 - Cidade 03', '00000-000', 2, 1, 0, 50, 200.0, null);
 
 insert into locacao(id, id_imovel, id_inquilino, ativo, data_fim, data_inicio, data_vencimento, perc_multa, valor_aluguel, obs) values (1, 1, 3, 2, '10-02-2022', '01-02-2022', '30-03-2022', 10, 150.0, 'Ficou com medo do fantasma.');
 
