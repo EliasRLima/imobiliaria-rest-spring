4e638c1de142d646788cd7d6e7762746e8e57428
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-14 19:37:34 -0300

    Create AluguelRepository.java

---
+++ src/main/java/ifma/edu/imobiliaria/repository/AluguelRepository.java
@@ -0,0 +1,14 @@
+package ifma.edu.imobiliaria.repository;
+
+import ifma.edu.imobiliaria.model.Aluguel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AluguelRepository extends JpaRepository<Aluguel, Long> {
+
+    List<Aluguel> findByImovelContaining(String id_imovel );
+    Page<Aluguel> findByImovelContaining(String id_imovel, Pageable paginacao);
+}
