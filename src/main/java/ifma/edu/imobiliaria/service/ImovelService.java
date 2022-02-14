20e6d0e71b5abefcd7493914ab653cdeeb5bcef7
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-13 21:55:19 -0300

    Update ImovelService.java

--- src/main/java/ifma/edu/imobiliaria/service/ImovelService.java
+++ src/main/java/ifma/edu/imobiliaria/service/ImovelService.java
@@ -3,8 +3,11 @@ package ifma.edu.imobiliaria.service;
 import ifma.edu.imobiliaria.model.Imovel;
 import ifma.edu.imobiliaria.repository.ImovelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -17,5 +20,28 @@ public class ImovelService {
         this.imovelRepository = imovelRepository;
     }
 
-    public List<Imovel> todos(){ return  imovelRepository.findAll()}
+    public List<Imovel> todos(){ return  imovelRepository.findAll();}
+
+    public List<Imovel> buscaPor(String nome) {
+        return imovelRepository.findByEndContaining(nome);
+    }
+
+    public Page<Imovel> buscaPor(String nome, Pageable paginacao) {
+        return imovelRepository.findByEndContaining(nome, paginacao);
+    }
+
+    @Transactional
+    public Imovel salva(Imovel cliente) {
+        return imovelRepository.save(cliente);
+    }
+
+
+    @Transactional
+    public void removePelo(Integer id) {
+        imovelRepository.deleteById(Long.valueOf(id));
+    }
+
+    public Page<Imovel> buscaCom(Pageable paginacao) {
+        return imovelRepository.findAll(paginacao );
+    }
 }
