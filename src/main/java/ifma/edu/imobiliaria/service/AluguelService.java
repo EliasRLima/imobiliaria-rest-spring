7d569b07a1162a54a017e49275011d1148151897
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-14 20:15:22 -0300

    Update AluguelService.java

--- src/main/java/ifma/edu/imobiliaria/service/AluguelService.java
+++ src/main/java/ifma/edu/imobiliaria/service/AluguelService.java
@@ -5,11 +5,13 @@ import ifma.edu.imobiliaria.repository.AluguelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AluguelService {
 
     private final AluguelRepository aluguelRepository;
