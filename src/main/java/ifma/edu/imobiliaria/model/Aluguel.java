28e18ae1dcef36741a6e156c7000104f336a6a7a
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-02-15 18:58:56 -0300

    Update Aluguel.java

--- src/main/java/ifma/edu/imobiliaria/model/Aluguel.java
+++ src/main/java/ifma/edu/imobiliaria/model/Aluguel.java
@@ -1,11 +1,14 @@
 package ifma.edu.imobiliaria.model;
 
 import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
+@Entity
 public class Aluguel {
 
     @Id
